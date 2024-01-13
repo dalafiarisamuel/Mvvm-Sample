b6bebce080b7926a1d6733b3c4847f7c889bb187
Dalafiari Samuel <dalafiarisamuel@gmail.com>
2024-01-13 15:38:56 +0100

    update

--- Mvvm Sample/Content-ViewModel.swift
+++ Mvvm Sample/Content-ViewModel.swift
@@ -13,15 +13,14 @@ extension ContentView {
         @Published var isTurnedOn: Bool = false
         @Published var counter: Int = 0
         @Published var itemList: [Item] = [Item]()
+        private let randomItemNames = ["Playstation", "Xbox", "Nintendo Wii", "Nintendo Switch", "Gameboy"]
 
         func increment() {
             counter += 1
         }
 
         func addItem() {
-            let randomItemNames = ["Playstation", "Xbox", "Nintendo Wii", "Nintendo Switch", "Gameboy"]
             let itemName = randomItemNames.randomElement()!
-
             let randomItem = Item(name: itemName, description: "Item: \(itemList.count + 1)")
 
             withAnimation {
